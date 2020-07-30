17be41742ab79c961ae2b718b10b43e0dbdc8276
Rhuddhi Gawas <58952410+RhuddhiGawas02@users.noreply.github.com>
2020-07-30 18:01:09 +0530

    Add files via upload

---
+++ Python Slicing of list.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+###  Slicing of list ###
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+Names=["Rhuddhi","Priyanka","Kalpehs","Shradhha","Sumit","Dhanashree","Sai","Trupti","Sahil","Akshitha","Nikhil"]
+
+
+# In[3]:
+
+
+type(Names)
+
+
+# In[4]:
+
+
+print(Names[-3])
+
+
+# In[5]:
+
+
+print(Names[3])
+
+
+# In[6]:
+
+
+len(Names)
+
+
+# In[ ]:
+
+
+#Slicing of list
+Syntax : listName[startvalue:stopvalue:stepcount]
+
+
+# In[9]:
+
+
+print(Names[0:1])#To fetch 1st element from the list 
+
+
+# In[10]:
+
+
+print(Names[0:5])#TO fetch 1st 4 element from the list
+
+
+# In[11]:
+
+
+print(Names[5:])#For printing 5th index to last index of list
+
+
+# In[13]:
+
+
+print(Names)
+
+
+# In[ ]:
+
+
+#to print alternate element
+
+
+# In[14]:
+
+
+print(Names[0: :2])#To print 2nd element
+
+
+# In[15]:
+
+
+print(Names[0: :3])#To print 3rd element
+
+
+# In[16]:
+
+
+print(Names[0:11:3])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
